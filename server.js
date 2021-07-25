3266f0c069ba0d41240e8a815a9fab80cbff8cf5
drogonly1 <drogonly1@gmail.com>
2021-07-25 13:01:29 +0700

    dd

--- server.js
+++ server.js
@@ -1,19 +1,56 @@
-/**
- * Created by trungquandev.com's author on 18/02/2020.
- * server.js
- */
-const express = require('express')
-const app = express()
-const initRoutes = require('./routes/web')
+//  NODEMAILER
+//  NODEMON
 
-// Cho phép lý dữ liệu từ form method POST
-app.use(express.urlencoded({extended: true}))
+const express = require('express');
+const app = express();
 
-// Khởi tạo các routes cho ứng dụng
-initRoutes(app)
+const nodemailer = require("nodemailer");
 
-// chọn một port mà bạn muốn và sử dụng để chạy ứng dụng tại local
-const port = 8017
-app.listen(port, () => {
-  console.log(`Hello trungquandev.com, I'm running at localhost:${port}/`)
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+app.use(express.json())
+
+app.post('/', (req, res)=>{
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'nodejsmailertest00@gmail.com',
+            pass: '442442aA!'
+        }
+    })
+
+    const output = `
+        <h2 style="color:red;">You have new contact request</h2>
+        <h3>Contact Details</h3>
+        <ul>
+            <li>Name: ${req.body.name}</li>
+            <li>Email: ${req.body.email}</li>
+            <li>Subject: ${req.body.subject}</li>
+        </ul>
+        <h3>Message</h3>
+        <p>${req.body.comments}</p>
+    `
+
+    const mailOptions = {
+        from: "Nodemailer Contact <nodejsmailertest00@gmail.com>",
+        to: 'bismainlearn@gmail.com',
+        subject: `Nodemailer Contact !!!`,
+        html: output
+    }
+    
+    transporter.sendMail(mailOptions, (error, info)=>{
+        if(error){
+            console.log(error);
+            res.send('error');
+        }
+        else{
+            console.log('Email sent: ' + info.response);
+            res.send('success')
+        }
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
 })
